c9c013f9f3eec9d67f71906c3b3e8d4ce8b5432f
Kur0T <surajprabhudesai09@gmail.com>
2026-03-30 14:41:07 +0530

    Add ISS visibility checker and email notification
    This script checks if the International Space Station (ISS) is visible from the user's location at night and sends an email notification if it is.

---
+++ iss.py
@@ -0,0 +1,55 @@
+import time
+import os
+import requests
+from datetime import datetime
+import smtplib
+
+MY_EMAIL = os.environ.get("MY_EMAIL)
+MY_PASSWORD = os.environ.get("MY_PASSWORD")
+MY_LAT = 18.494190 # Your latitude
+MY_LONG = 73.848320 # Your longitude
+
+def iss_is_visible():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+    # Your position is within +5 or -5 degrees of the ISS position.
+    if MY_LAT-5<= iss_latitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <=MY_LONG+5:
+        return True
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now().hour
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
+while True:
+    time.sleep(300)
+    if iss_is_visible() and is_night():
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.login(user=MY_EMAIL,password=MY_PASSWORD)
+            connection.starttls()
+            if iss_is_visible():
+                connection.send_message(from_addr=MY_EMAIL,
+                                        to_addrs="surajprabhudesai1@gmail.com",
+                                        msg="Subject:Look Up!\n\nThe ISS is overhead your location!")
+#If the ISS is close to my current position
+# and it is currently dark
+# Then send me an email to tell me to look up.
+# BONUS: run the code every 60 seconds.
+
+
+
